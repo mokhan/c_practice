72ccaacaecc74103a3ad4915c85ed039477e06dc
mo k <mo@mokhan.ca>
2012-02-17 16:29:23 -0700

    reverse a string using pointer arithmetic.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void reverse(char* input){
+  char *begin, *end;
+  char tmp;
+  int i;
+
+  begin = &input[0];
+  end = &input[strlen(input)-1];
+  for (i = 0; i < strlen(input)-1; i++) {
+    if(begin == end) break;
+    tmp = *end;
+    *end = *begin;
+    *begin = tmp;
+    begin++;
+    end--;
+  }
+}
 
 int main(int argc, const char *argv[])
 {
-  puts("hello world");
+  unsigned char input[128];
+
+  /*printf("what is your name: ");*/
+  /*scanf("%s", input);*/
+
+  printf("enter a string to reverse: ");
+  scanf("%s", input);
+
+  reverse(input);
+  printf("%s", input);
+
+  /*printf("goodbye %s", input);*/
   return 0;
 }
